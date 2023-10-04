8610f2e1e79496da53dbe76782e8fe6c77105aef
Jonathan-Rocha <johnrocha149@gmail.com>
2023-10-03 21:15:16 -0300

    first commit

---
+++ src/coincontrol/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace coincontrol.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
