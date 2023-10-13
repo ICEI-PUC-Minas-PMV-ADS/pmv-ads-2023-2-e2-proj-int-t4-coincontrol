48fbb4b185de498a0a1f35ec72e75e2ca13aa125
TIAGO CARVALHO TAVEIRA ARAUJO <125419720+TiagoAraujo12@users.noreply.github.com>
2023-10-13 15:39:37 -0300

    Criando classe de categoria

---
+++ src/coincontrol/Models/Categoria/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace coincontrol.Models.categoria
+{
+    [Table("categoria")]
+    public class categoria
+    {
+        [Key]
+        [Column("idCategoria")]
+        public int IdCategoria { get; set; }
+
+        [Column("nome")]
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        [MaxLength(45)]
+        public string Nome { get; set; }
+
+        [Column("limite")]
+        [Required(ErrorMessage = "Obrigatório informar o limite de gastos!")]
+        [MaxLength(10)]
+        public float Email { get; set; }
+
+    }
+}
