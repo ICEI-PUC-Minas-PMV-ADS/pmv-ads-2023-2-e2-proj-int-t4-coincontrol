59d44fa651b72796ccb52d56956e72ac3f1a50bb
ChrisCuri <christianecuri@gmail.com>
2023-10-18 10:48:26 -0300

    Inserção do model Transação.

--- src/coincontrol/Models/Carteira/Carteiras.cs
+++ src/coincontrol/Models/Carteira/Carteiras.cs
@@ -1,18 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MvcCarteira.Models;
-
-public class Carteira
+namespace MvcCarteira.Models
 {
-    [Key]
-    public int IdCarteira { get; set; }
-    public float SaldoInicial { get; set; }
-    public float SaldoParcial { get; set; }
+    public class Carteira
+    {
+        [Key]
+        [Column("idCarteira")]
+        public int IdCarteira { get; set; }
 
-    [Required(ErrorMessage = "Obrigatório informar o ano.")]
-    public int Ano { get; set; }
+        [Column("ano")]
+        [Required(ErrorMessage = "Obrigatório informar o ano.")]
+        public int Ano { get; set; }
 
-    [Required(ErrorMessage = "Obrigatório informar o mês.")]
-    public int Mes { get; set; }
+        [Column("mes")]
+        [Required(ErrorMessage = "Obrigatório informar o mês.")]
+        public int Mes { get; set; }
 
+        [Column("saldoInicial")]
+        public float SaldoInicial { get; set; }
+
+        [Column("saldoParcial")]
+        public float SaldoParcial { get; set; }
+
+        [Column("idUsuario")]
+        public int IdUsuario { get; set; }
+
+    }
 }
---
+++ src/coincontrol/Models/Transacoes/Transacao.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace coincontrol.Models.Transacoes
+{
+    [Table("transacoes")]
+    public class Transacao
+    {
+        [Key]
+        [Column("idTransacoes")]
+        public int IdTransacoes { get; set; }
+
+        [Column("idCarteira")]
+        [Required(ErrorMessage = "Obrigatório informar a carteira!")]
+        public int IdCarteira { get; set; }
+
+        [Column("idUsuario")]
+        public int IdUsuario { get; set; }
+
+        [Column("valor")]
+        [Required(ErrorMessage = "Obrigatório informar o valor!")]
+        public float Valor { get; set; }
+
+        [Column("date")]
+        [Required(ErrorMessage = "Obrigatório informar a data!")]
+        public DateTime Date { get; set; }
+
+        [Column("idCategoria")]
+        [Required(ErrorMessage = "Obrigatório informar a categoria!")]
+        public int IdCategoria { get; set; }
+
+        [Column("saldoInicial")]
+        public float SaldoInicial { get; set; }
+
+        [Column("saldoFinal")]
+        public float SaldoFinal { get; set; }
+
+        [Column("moeda")]
+        public char Moeda { get; set; }
+    }
+}
+
