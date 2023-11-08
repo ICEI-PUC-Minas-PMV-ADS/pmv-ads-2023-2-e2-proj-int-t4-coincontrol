77e47e4122ba6aaa5cda36cd26e53122fd543065
Ana Maria Pessoa <pessoaana.m@gmail.com>
2023-11-08 04:20:59 -0300

    feat: implementacao da carteira

--- src/coincontrol/Models/Carteira/Carteiras.cs
+++ src/coincontrol/Models/Carteira/Carteiras.cs
@@ -1,30 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MvcCarteira.Models
+namespace coincontrol.Models.Carteira
 {
-    public class Carteira
+    [Table("carteiras")]
+    public class Carteiras
     {
         [Key]
-        [Column("idCarteira")]
+        [Column("IdCarteira")]
         public int IdCarteira { get; set; }
 
-        [Column("ano")]
+        [Column("IdUsuario")]
+        public int IdUsuario { get; set; }
+
+        [Column("SaldoInicial")]
+        public float SaldoInicial { get; set; }
+
+        [Column("SaldoParcial")]
+        public float SaldoParcial { get; set; }
+
+        [Column("Ano")]
         [Required(ErrorMessage = "Obrigatório informar o ano.")]
         public int Ano { get; set; }
 
-        [Column("mes")]
+        [Column("Mes")]
         [Required(ErrorMessage = "Obrigatório informar o mês.")]
         public int Mes { get; set; }
-
-        [Column("saldoInicial")]
-        public float SaldoInicial { get; set; }
-
-        [Column("saldoParcial")]
-        public float SaldoParcial { get; set; }
-
-        [Column("idUsuario")]
-        public int IdUsuario { get; set; }
-
     }
 }
+
