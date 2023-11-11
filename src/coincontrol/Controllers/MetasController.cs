ca11caa0aec39fc9988238e4625cf13422314a42
Jonathan-Rocha <johnrocha149@gmail.com>
2023-11-11 19:58:00 -0300

    Criação do modelo de metas

---
+++ src/coincontrol/Controllers/MetasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using coincontrol.CCDbContext;
+using coincontrol.Models.Meta;
+using System.Security.Claims;
+
+namespace coincontrol.Controllers
+{
+    public class MetasController : Controller
+    {
+        private readonly CoinControlBdContext _context;
+
+        public MetasController(CoinControlBdContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Metas
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Metas.ToListAsync());
+        }
+
+        // GET: Metas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Metas == null)
+            {
+                return NotFound();
+            }
+
+            var meta = await _context.Metas
+                .FirstOrDefaultAsync(m => m.IdMeta == id);
+            if (meta == null)
+            {
+                return NotFound();
+            }
+
+            return View(meta);
+        }
+
+        // GET: Metas/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCategoria"] = new SelectList(_context.Categorias, "IdCategoria", "Nome");
+            return View();
+        }
+
+        // POST: Metas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ValorTotal")] Meta meta)
+        {
+            var emailUsuario = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == emailUsuario);
+
+            if (ModelState.IsValid)
+            {
+                meta.IdUsuario = usuario.IdUsuario;
+                _context.Add(meta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categorias, "IdCategoria", "Nome", meta.IdCategoria);
+            return View(meta);
+        }
+
+        // GET: Metas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Metas == null)
+            {
+                return NotFound();
+            }
+
+            var meta = await _context.Metas.FindAsync(id);
+            if (meta == null)
+            {
+                return NotFound();
+            }
+            return View(meta);
+        }
+
+        // POST: Metas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ValorTotal,IdCategoria")] Meta meta)
+        {
+            if (id != meta.IdMeta)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MetaExists(meta.IdMeta))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meta);
+        }
+
+        // GET: Metas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Metas == null)
+            {
+                return NotFound();
+            }
+
+            var meta = await _context.Metas
+                .FirstOrDefaultAsync(m => m.IdMeta == id);
+            if (meta == null)
+            {
+                return NotFound();
+            }
+
+            return View(meta);
+        }
+
+        // POST: Metas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Metas == null)
+            {
+                return Problem("Entity set 'CoinControlBdContext.Meta'  is null.");
+            }
+            var meta = await _context.Metas.FindAsync(id);
+            if (meta != null)
+            {
+                _context.Metas.Remove(meta);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MetaExists(int id)
+        {
+          return _context.Metas.Any(e => e.IdMeta == id);
+        }
+    }
+}
---
+++ src/coincontrol/Models/Meta/Meta.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace coincontrol.Models.Meta
+{
+    [Table("metas")]
+    public class Meta
+    {
+        [Key]
+        [Column("idMeta")]
+        public int IdMeta { get; set; }
+
+        [Column("valorTotal")]
+        [Required(ErrorMessage = "Obrigatório informar o valor da meta!")]
+        public float ValorTotal { get; set; }
+
+        [Column("valorParcial")]
+        public float ValorParcial { get; set; }
+
+        [Column("idUsuario")]
+        public int IdUsuario { get; set; }
+
+        [Column("idCategoria")]
+        [Required(ErrorMessage = "Obrigatório informar a categoria!")]
+        public int IdCategoria { get; set; }
+
+    }
+}
