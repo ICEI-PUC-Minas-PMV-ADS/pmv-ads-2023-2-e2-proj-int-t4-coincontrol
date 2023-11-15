38d7183250d3cb425966767f6ef7ab7c7c9117b3
Gabriel S. Lourenço <127675409+g413rie1@users.noreply.github.com>
2023-11-14 22:57:27 -0300

    Add files via upload

---
+++ src/coincontrol/Models/Category.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace coincontrol.Models
+{
+    public class Category
+    {
+        [Key] //primary key
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "Favor inserir Título.")]
+        public string Título { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Ícone { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Modalidade { get; set; } = "Saída";
+
+        [NotMapped]
+        public string? TituloComIcone
+        {
+            get
+            {
+                return this.Ícone + " " + this.Título;
+            }
+        }
+    }
+}
---
+++ src/coincontrol/Models/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace coincontrol.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; } // Temos que salvar a CategoryId nessa Transaction entity
+
+        [Range(1, int.MaxValue, ErrorMessage = "Favor selecionar uma categoria.")]
+
+        public int CategoryId { get; set; }
+        public Category? Categoria { get; set; }
+       
+        [Range(1, int.MaxValue, ErrorMessage = "O valor deve ser acima de R$0.")]
+
+        public int Valor { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Descrição { get; set; }
+
+        public DateTime Data { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string? TituloCategoriaComIcone
+        {
+            get
+            {
+                return Categoria == null ? "" : Categoria.Ícone + " " + Categoria.Título;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Categoria == null || Categoria.Modalidade == "Saída") ? "- " : "+ ") + Valor.ToString("C0");
+            }
+        }
+    }
+}
