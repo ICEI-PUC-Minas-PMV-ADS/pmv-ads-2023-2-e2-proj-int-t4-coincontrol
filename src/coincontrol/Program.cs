b24eda65e5a2adc292453f9487b607d69e2cd222
Gabriel S. Lourenço <127675409+g413rie1@users.noreply.github.com>
2023-11-28 18:35:24 -0300

    Update Syncfusion License Key

--- src/coincontrol/Program.cs
+++ src/coincontrol/Program.cs
@@ -18,7 +18,7 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 });
 
 //Register Syncfusion license
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpRGtGfV5yd0VAal9STnNZUj0eQnxTdEZiWH5fcXxWRWVaUEVxVw==");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx0TXxbf1xzZFFMY19bQHdPIiBoS35RdURiW3dfdndWQmJbU0F3");
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
